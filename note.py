f6aaa9565031e17e9247c10b1fcf5035b55bdc06
Falk Werner <falk.werner@gmx.net>
2023-01-14 14:54:31 +0100

    removed commented-out line

--- note.py
+++ note.py
@@ -547,7 +547,6 @@ class NoteFrame(ttk.Frame):
         self.frame = HtmlFrame(self.notebook, messages_enabled=False)
         self.frame.on_link_click(self.link_clicked)
         self.frame.load_html("")
-        #self.frame.grid(column=0, row=0, sticky=tk.NSEW)
         self.notebook.add(self.frame, 'View')
 
         editframe = tk.Frame(self.notebook)
